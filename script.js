b8d886cb49ff0a00f05574df62eca5506f76ec5d
Ravi Suman <101641600+ravisuman903@users.noreply.github.com>
2026-07-10 13:29:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const button = document.querySelector(".hero button");
-
-    button.addEventListener("click", function () {
-        window.open("https://wa.me/917733816532", "_blank");
-    });
+    console.log("Radhe Radhe Bhog Heeng Website Loaded Successfully!");
 });
