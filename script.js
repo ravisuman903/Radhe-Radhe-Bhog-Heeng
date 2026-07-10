e3d5e0e90038d329cf761d4494a8b33808af2f8b
Ravi Suman <101641600+ravisuman903@users.noreply.github.com>
2026-07-10 13:01:16 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const button = document.querySelector(".hero button");
+
+    button.addEventListener("click", function () {
+        window.open("https://wa.me/917733816532", "_blank");
+    });
+});
