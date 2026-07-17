951fb2a0a8fcd03a5cd377abdadf9632247416c2
Ravi Suman <101641600+ravisuman903@users.noreply.github.com>
2026-07-17 13:15:51 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -8,16 +8,15 @@ import {
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "...",
-  authDomain: "...",
-  projectId: "...",
-  storageBucket: "...",
-  messagingSenderId: "...",
-  appId: "..."
+  apiKey: "AIzaSyDHFc3TuBAjAhAWrmVzQdX5eLFoKTYulmI",
+  authDomain: "radhe-radhe-bhog-heeng.firebaseapp.com",
+  projectId: "radhe-radhe-bhog-heeng",
+  storageBucket: "radhe-radhe-bhog-heeng.firebasestorage.app",
+  messagingSenderId: "438530440845",
+  appId: "1:438530440845:web:c222af6c9319d2690bacc4"
 };
 
 const app = initializeApp(firebaseConfig);
-
 const db = getFirestore(app);
 
 export { db, collection, addDoc, getDocs };
