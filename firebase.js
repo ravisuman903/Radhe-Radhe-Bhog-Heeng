07c9e30eafed8032d476a216f4723880be093bc1
Ravi Suman <101641600+ravisuman903@users.noreply.github.com>
2026-07-21 18:05:16 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,39 +1,77 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 
 import {
-getFirestore,
-collection,
-addDoc,
-getDocs,
-onSnapshot,
-query,
-where,
-deleteDoc,
-doc,
-updateDoc
-}
-from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+    getFirestore,
+    collection,
+    addDoc,
+    getDocs,
+    onSnapshot,
+    query,
+    where,
+    deleteDoc,
+    doc,
+    updateDoc
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+
+// ===============================
+// FIREBASE CONFIGURATION
+// ===============================
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDHFc3TuBAjAhAWrmVzQdX5eLFoKTYulmI",
-  authDomain: "radhe-radhe-bhog-heeng.firebaseapp.com",
-  projectId: "radhe-radhe-bhog-heeng",
-  storageBucket: "radhe-radhe-bhog-heeng.firebasestorage.app",
-  messagingSenderId: "438530440845",
-  appId: "1:438530440845:web:c222af6c9319d2690bacc4"
+
+    apiKey: "AIzaSyDHFc3TuBAjAhAWrmVzQdX5eLFoKTYulmI",
+
+    authDomain:
+        "radhe-radhe-bhog-heeng.firebaseapp.com",
+
+    projectId:
+        "radhe-radhe-bhog-heeng",
+
+    storageBucket:
+        "radhe-radhe-bhog-heeng.firebasestorage.app",
+
+    messagingSenderId:
+        "438530440845",
+
+    appId:
+        "1:438530440845:web:c222af6c9319d2690bacc4"
 };
 
+
+// ===============================
+// INITIALIZE FIREBASE
+// ===============================
+
 const app = initializeApp(firebaseConfig);
+
+
+// ===============================
+// INITIALIZE FIRESTORE
+// ===============================
+
 const db = getFirestore(app);
 
+
+// ===============================
+// EXPORT FIREBASE FUNCTIONS
+// ===============================
+
 export {
-  db,
-  collection,
-  addDoc,
-  getDocs,
-  onSnapshot,
-  query,
-  where,
-  deleteDoc,
-  doc,
-  updateDoc
+
+    db,
+
+    collection,
+    addDoc,
+    getDocs,
+
+    onSnapshot,
+
+    query,
+    where,
+
+    deleteDoc,
+    doc,
+    updateDoc
+
 };
