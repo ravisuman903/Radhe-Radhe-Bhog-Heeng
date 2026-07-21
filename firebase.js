09522ebbcc9a0b69ac7157c8b1c57caedf2a4229
Ravi Suman <101641600+ravisuman903@users.noreply.github.com>
2026-07-21 18:51:24 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -13,65 +13,28 @@ import {
     updateDoc
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
-
-// ===============================
-// FIREBASE CONFIGURATION
-// ===============================
-
 const firebaseConfig = {
-
     apiKey: "AIzaSyDHFc3TuBAjAhAWrmVzQdX5eLFoKTYulmI",
-
-    authDomain:
-        "radhe-radhe-bhog-heeng.firebaseapp.com",
-
-    projectId:
-        "radhe-radhe-bhog-heeng",
-
-    storageBucket:
-        "radhe-radhe-bhog-heeng.firebasestorage.app",
-
-    messagingSenderId:
-        "438530440845",
-
-    appId:
-        "1:438530440845:web:c222af6c9319d2690bacc4"
+    authDomain: "radhe-radhe-bhog-heeng.firebaseapp.com",
+    projectId: "radhe-radhe-bhog-heeng",
+    storageBucket: "radhe-radhe-bhog-heeng.firebasestorage.app",
+    messagingSenderId: "438530440845",
+    appId: "1:438530440845:web:c222af6c9319d2690bacc4"
 };
-
-
-// ===============================
-// INITIALIZE FIREBASE
-// ===============================
 
 const app = initializeApp(firebaseConfig);
 
-
-// ===============================
-// INITIALIZE FIRESTORE
-// ===============================
-
 const db = getFirestore(app);
 
-
-// ===============================
-// EXPORT FIREBASE FUNCTIONS
-// ===============================
-
 export {
-
     db,
-
     collection,
     addDoc,
     getDocs,
-
     onSnapshot,
-
     query,
     where,
-
     deleteDoc,
     doc,
     updateDoc
-
 };
